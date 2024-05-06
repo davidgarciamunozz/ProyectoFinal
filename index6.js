c6854fe0413e59e02e7d66c17eb32aabc7bcd350
davidgarciamunozz <garxia0710@gmail.com>
2024-05-06 17:33:01 -0500

    dataUpdated/featureDavid

--- index6.js
+++ index6.js
@@ -1,8 +1,10 @@
 import { obtenerDoctores, Doctor } from "./utils.js";
 
 
-const render = async () => {
-    
+const render = async (textoBusqueda) => {
+
+// const textoLimpio = textoBusqueda.toLowerCase();
+// console.log(textoLimpio);    
 const doctores = await obtenerDoctores();
 
 const doctor_box = document.getElementById("main");
@@ -11,7 +13,20 @@ for (const doctor of doctores) {
     const doctorRenders = new Doctor(doctor.firstname, doctor.lastname, doctor.genre, doctor.role, doctor.available, doctor.image, doctor.id, doctor.dateOfBirth, doctor.maritalStatus, doctor.emailAddress, doctor.location, doctor.phoneNumber);
     const doctorRender = doctorRenders.render();
     doctor_box.appendChild(doctorRender);
- }
+ };
+
+ if(textoLimpio === "" || doctor.firstame.toLowerCase().includes(textoLimpio)){
+    contenedor.appendChild(agenteRender);
+    agente.addEventListeners();
+}
+
+//  const barraBusqueda = document.getElementById("larger");
+//  barraBusqueda.addEventListener("input", async (event)=>{
+//     const textBusqueda = event.target.value;
+//     await render(textBusqueda);
+//  });
+
+ 
 
 
 
